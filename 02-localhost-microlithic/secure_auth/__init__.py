50091be0ba6d414b95c7e47f9c54a1e23b3fbdb3
Adrien SIEG <31514820+adriensieg@users.noreply.github.com>
2025-09-24 09:23:00 -0500

    Update __init__.py

--- 02-localhost-microlithic/secure_auth/__init__.py
+++ 02-localhost-microlithic/secure_auth/__init__.py
@@ -1,1 +1,7 @@
+"""Secure OAuth Authentication Library for FastAPI."""
 
+from .config import Settings, get_settings
+from .stores import StateStore, JWKSCache
+from .middleware import setup_middleware, setup_error_handlers
+
+__all__ = ['Settings', 'get_settings', 'StateStore', 'JWKSCache', 'setup_middleware', 'setup_error_handlers']
